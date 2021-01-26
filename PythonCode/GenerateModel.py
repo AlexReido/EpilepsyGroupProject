7e2717f6569540126040b9a835f3f36dd65ac56d
AlexReido <areid.1028@gmail.com>
2021-01-26 19:17:20 +0000

    added distance based network generation and unit test suite function for quicker tests

--- PythonCode/GenerateModel.py
+++ PythonCode/GenerateModel.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 import bct
+import numpy as np
+import matlab.engine
+
 app = FastAPI()
+
 
 def getStructuralnetwork(k, m, structure):
     if structure == "random":
         return bct.makerandCIJ_und(k, m)
     elif structure == "lattice":
         return bct.makeringlatticeCIJ(k, m)
+    elif structure == "toeplitz":
+        return bct.maketoeplitzCIJ(k, m, 1.5)
     elif structure == "smallword":
-        return None#TODO small world
+        return True #TODO small world
     else:
-        raise ValueError("Network type" + type + "not supported")
+        raise ValueError("Network type: " + type + "not supported")
 
 
 
@@ -19,8 +25,34 @@ def getStructuralnetwork(k, m, structure):
 async def generateNetwork(nodes: int = 20, edges: int = 100, structure: str = "random"):
     # weighted undirected network
     model = getStructuralnetwork(nodes, edges, structure)
+    # functional = bct.motif3funct_bin(model)
+    distance = bct.distance_wei_floyd(model, 'log')
+    # print("ditancee:")
+    # print(type(distance))
+    # print(distance)
+    # print("first")
+    print(distance[2])
+    print(model)
+    max = np.amax(distance)
+    # print(max)
+    # transform = lambda x: x/max
+    func = np.divide(distance, max)
+    func = np.around(func, 3)
+    # print(func)
+    # eng = matlab.engine.start_matlab()
+    # print(type(model))
+    # print(type(func))
+    # print(type(distance))
+    # # SC FC ED pred_var model
+    predvar = ['ED']#,'SPLwei_log','SI','T']
+    # predvar = matlab.double(predvar)
+    # bct.z
+    # res = eng.predict_fc(matlab.double(model.tolist()), matlab.double(func.tolist()),
+    #                      matlab.double(distance[2].tolist()), predvar, nargout=5)
+    # print(res)
     # functionalConnectivity = bct.p
-    print(type(model))
-    listmodel = model.tolist()
-    return listmodel
+    # print(functional)
+    # print(type(model))
+    listmodel = func.tolist()
+    return listmodel[2]
 
--- PythonCode/Tests/suite.py
+++ PythonCode/Tests/suite.py
@@ -1,6 +1,17 @@
 import unittest
 
-
+def test_units():
+    """
+    Run smaller tests with unit in the name (run more often and quicker)
+    :return:
+    """
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    tests = loader.discover(start_dir='.', pattern='test_unit*.py')
+    suite.addTests(tests)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+    
 def test_suite():
     """
     Run all of the test cases in a test suite.
