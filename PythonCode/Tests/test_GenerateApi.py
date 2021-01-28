787c16bd35a4655b175d5f09bbc9990ab014a280
AlexReido <areid.1028@gmail.com>
2021-01-28 16:19:38 +0000

    added testing for all types of networks

--- PythonCode/Tests/test_GenerateApi.py
+++ PythonCode/Tests/test_GenerateApi.py
@@ -1,10 +1,10 @@
 import unittest
 from multiprocessing import Process
 # from .. import GenerateModel
-import asyncio
 import uvicorn
 import requests
 import time
+import bct
 
 
 class TestGenerate(unittest.TestCase):
@@ -17,12 +17,12 @@ class TestGenerate(unittest.TestCase):
                                 "log_level": "info"},
                             daemon=True)
         self.proc.start()
-        time.sleep(4)
+        time.sleep(8)
 
     def tearDown(self):
         self.proc.terminate()
 
-    def test_ArtificialApi(self):
+    def test_ArtificialRandom(self):
         response = requests.get('http://127.0.0.1:5000/model/artificial/',
                                 params={'nodes': 30, 'edges': 200, 'structure': 'random'})
         self.assertEqual(response.status_code, 200)
@@ -32,6 +32,29 @@ class TestGenerate(unittest.TestCase):
         print(type(net))
         print(net)
         self.assertEqual(len(net), 30)
+
+    def test_ArtificialLattice(self):
+        response = requests.get('http://127.0.0.1:5000/model/artificial/',
+                                params={'nodes': 30, 'edges': 200, 'structure': 'lattice'})
+        self.assertEqual(response.status_code, 200)
+        print(response)
+        print(response.headers['content-type'])
+        net = response.json()
+        print(type(net))
+        print(net)
+        self.assertEqual(len(net), 30)
+        print(bct.assortativity_wei(net))
+
+    def test_ArtificialToeplitz(self):
+        response = requests.get('http://127.0.0.1:5000/model/artificial/',
+                                params={'nodes': 30, 'edges': 200, 'structure': 'toeplitz'})
+        self.assertEqual(200, response.status_code) # (Expected, actual)
+        print(response)
+        print(response.headers['content-type'])
+        net = response.json()
+        print(type(net))
+        print(net)
+        self.assertEqual(30, len(net))
 
 
 if __name__ == '__main__':
