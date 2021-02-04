9f2677647abffd8385c5b363ded50579a7e6d0e2
AlexReido <areid.1028@gmail.com>
2021-02-04 18:23:55 +0000

    added the options for nodes and edges as well as help for command

--- PythonCode/TyperCLI/TyperGenerateArtificial.py
+++ PythonCode/TyperCLI/TyperGenerateArtificial.py
@@ -1,20 +1,24 @@
-# import bct
 import typer
 from PythonCode.GenerateArtificial.GenerateModel import get_artificial_net
 
 
-# from .PythonCode.GenerateArtificial.GenerateModel import get_artificial_net
-
-def getnetwork(type):
-    nodes = 10
-    edges = 30
+def getnetwork(type, nodes, edges):
     net = get_artificial_net(nodes, edges, type)
     # print(net)
     return net
 
 
-def main(type: str):
-    network = getnetwork(type)
+def main(
+        type: str = typer.Argument(..., help="Type of structural network: random, lattice or toeplitz"),
+        n: int = typer.Option(10, help="Number of nodes in network"),
+        e: int = typer.Option(30, help="Number of edges in network")
+):
+    """
+    Generate a network based on the type and optional nodes and edges provided \n
+    Type should be either random, lattice or toeplitz. \t
+    Prints network to console.
+    """
+    network = getnetwork(type, n, e)
     typer.echo(network)
 
 
